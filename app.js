4a752fb024426368ebe102faea9e2cdc8bbb6b90
jeffsford <jeff@jeffford.com>
2017-03-07 15:31:31 -0800

    Updated version of the JS file

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ var firstQuestion = prompt('Was I born in the 1970s?');
 if (firstQuestion.toUpperCase() === 'YES' || firstQuestion.toUpperCase() === 'Y') {
     console.log('I was not born in the 1970s, but you were very close. I was born in April of 1980.');
 } else if (firstQuestion.toUpperCase() === 'NO' || firstQuestion.toUpperCase() === 'N') {
-    console.log('You are correct!  I just barely missed the 1970s, being born in April of 1980.')
+    console.log('You are correct!  I just barely missed the 1970s, being born in April of 1980.');
 } else {
-    console.log();('You did\'nt put in a proper answer!')
+    console.log();('You didn\'t enter a valid response!');
 }
 
 var secondQuestion = prompt('Have I lived anywhere outside of Washington state in my life?');
 
 if (secondQuestion.toUpperCase() === 'YES' || secondQuestion.toUpperCase() === 'Y') {
-    console.log('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.')
+    console.log('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.');
 } else if (secondQuestion.toUpperCase() === 'NO' || secondQuestion.toUpperCase() === 'N') {
-    console.log('Actually I have been fortunate to live several different places in my life.')
+    console.log('Actually I have been fortunate to live several different places in my life.');
+} else {
+    console.log();('You didn\'t enter a valid response!');
 }
 
-var thirdQuestion = prompt('')
+var thirdQuestion = prompt('Did I attend college after graduating from high school?');
+
+if (thirdQuestion.toUpperCase() === 'YES' || thirdQuestion.toUpperCase() === 'Y') {
+    console.log('That\'s correct.  I attended the University of Washington, and studied Political Science.');
+  } else if (thirdQuestion.toUpperCase() === 'NO' || thirdQuestion.toUpperCase() === 'N') {
+    console.log('You are incorrect.  I did attend college after graduating from high school.')
+  } else {
+    console.log();('You didn\'t enter a valid response!');
+}
+
+var fourthQuestion = prompt()
