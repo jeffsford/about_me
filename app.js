63e0fbf3d2017219321e6c7e1d26edc0f0a6d0ae
jeffsford <jeff@jeffford.com>
2017-03-08 15:10:21 -0800

    Added variables to js code

--- app.js
+++ app.js
@@ -1,108 +1,80 @@
-'use strict'
+'use strict';
 
 var userName = prompt('What is your name?');
-    alert('It\'s nice to meet you ' + userName + '!');
+  alert('It\'s nice to meet you ' + userName + '!');
 
 var firstQuestion = prompt('Was I born in the 1970s?');
+var answerI = 'You didn\'t enter a valid response!';
 
 if (firstQuestion.toUpperCase() === 'YES' || firstQuestion.toUpperCase() === 'Y') {
-
-    alert('I was not born in the 1970s, but you were very close. I was born in April of 1980.');
-
-    console.log('I was not born in the 1970s, but you were very close. I was born in April of 1980.');
-
+var answerOneY = 'I was not born in the 1970s, but you were very close. I was born in April of 1980.';
+  alert(answerOneY);
+  console.log(answerOneY);
 } else if (firstQuestion.toUpperCase() === 'NO' || firstQuestion.toUpperCase() === 'N') {
-
-    alert('You are correct!  I just barely missed the 1970s, being born in April of 1980.');
-
-    console.log('You are correct!  I just barely missed the 1970s, being born in April of 1980.');
+var answerOneN = 'You are correct!  I just barely missed the 1970s, being born in April of 1980.';
+  alert(answerOneN);
+  console.log(answerOneN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var secondQuestion = prompt('Have I lived anywhere outside of Washington state in my life?');
 
 if (secondQuestion.toUpperCase() === 'YES' || secondQuestion.toUpperCase() === 'Y') {
-
-    alert('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.');
-
-    console.log('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.');
-
+var answerTwoY = 'That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.';
+  alert(answerTwoY);
+  console.log(answerTwoY);
 } else if (secondQuestion.toUpperCase() === 'NO' || secondQuestion.toUpperCase() === 'N') {
-
-    alert('Actually I have been fortunate to live several different places in my life.');
-
-    console.log('Actually I have been fortunate to live several different places in my life.');
-
+var answerTwoN = 'Actually I have been fortunate to live several different places in my life.';
+  alert(answerTwoN);
+  console.log(answerTwoN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var thirdQuestion = prompt('Did I attend college after graduating from high school?');
 
 if (thirdQuestion.toUpperCase() === 'YES' || thirdQuestion.toUpperCase() === 'Y') {
-
-    alert('That\'s correct.  I attended the University of Washington, and studied Political Science.');
-
-    console.log('That\'s correct.  I attended the University of Washington, and studied Political Science.');
-
+var answerThreeY = 'That\'s correct.  I attended the University of Washington, and studied Political Science.';
+    alert(answerThreeY);
+    console.log(answerThreeY);
 } else if (thirdQuestion.toUpperCase() === 'NO' || thirdQuestion.toUpperCase() === 'N') {
-
-    alert('You are incorrect.  I did attend college after graduating from high school.');
-
-    console.log('You are incorrect.  I did attend college after graduating from high school.');
-
+var answerThreeN = 'You are incorrect.  I did attend college after graduating from high school.';
+  alert(answerThreeN);
+  console.log(answerThreeN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var fourthQuestion = prompt('Do I have any siblings?');
 
 if (fourthQuestion.toUpperCase() === 'YES' || fourthQuestion.toUpperCase() === 'Y') {
-
-    alert('Yes. I do have siblings.');
-
-    console.log('Yes. I do have siblings.');
-
+var answerFourY = 'Yes. I do have siblings.';
+  alert(answerFourY);
+  console.log(answerFourY);
 } else if (fourthQuestion.toUpperCase() === 'NO' || fourthQuestion.toUpperCase() === 'N') {
-
-    alert('Incorrect. I do have siblings.');
-
-    console.log('Incorrect. I do have siblings.');
-
+var answerFourN = 'Incorrect. I do have siblings.';
+    alert(answerFourN);
+    console.log(answerFourN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var fifthQuestion = prompt('Am I right-handed?');
 
 if (fifthQuestion.toUpperCase() === 'YES' || fifthQuestion.toUpperCase() === 'Y') {
-
-    alert('No! I am left-handed.');
-
-    console.log('No! I am left-handed.');
-
+var answerFiveY = 'No! I am left-handed.';
+  alert(answerFiveY);
+  console.log(answerFiveY);
 } else if (fifthQuestion.toUpperCase() === 'NO' || fifthQuestion.toUpperCase() === 'N') {
-
-    alert('That is correct!');
-
-    console.log('That is correct!');
-
+var answerFiveN = 'That is correct!';
+  alert(answerFiveN);
+  console.log(answerFiveN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
