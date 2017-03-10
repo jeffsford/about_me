3ab638bcdb162d0c33cd74c576f81405cfd2bba2
jeffsford <jeff@jeffford.com>
2017-03-09 16:00:13 -0800

    Added current code for files

--- app.js
+++ app.js
@@ -1,108 +1,141 @@
-'use strict'
+'use strict';
 
-var userName = prompt('What is your name?');
-    alert('It\'s nice to meet you ' + userName + '!');
+var userName = prompt('What is your name?');alert('It\'s nice to meet you ' + userName + '!');
 
 var firstQuestion = prompt('Was I born in the 1970s?');
+var answerI = 'You didn\'t enter a valid response!';
+var numCorrect = 0;
 
 if (firstQuestion.toUpperCase() === 'YES' || firstQuestion.toUpperCase() === 'Y') {
-
-    alert('I was not born in the 1970s, but you were very close. I was born in April of 1980.');
-
-    console.log('I was not born in the 1970s, but you were very close. I was born in April of 1980.');
-
+  var answerOneY = 'I was not born in the 1970s, but you were very close. I was born in April of 1980.';
+  alert(answerOneY);
+  console.log(answerOneY);
 } else if (firstQuestion.toUpperCase() === 'NO' || firstQuestion.toUpperCase() === 'N') {
-
-    alert('You are correct!  I just barely missed the 1970s, being born in April of 1980.');
-
-    console.log('You are correct!  I just barely missed the 1970s, being born in April of 1980.');
+  var answerOneN = 'You are correct!  I just barely missed the 1970s, being born in April of 1980.';
+  numCorrect++;
+  alert(answerOneN);
+  console.log(answerOneN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var secondQuestion = prompt('Have I lived anywhere outside of Washington state in my life?');
 
 if (secondQuestion.toUpperCase() === 'YES' || secondQuestion.toUpperCase() === 'Y') {
-
-    alert('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.');
-
-    console.log('That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.');
-
+  var answerTwoY = 'That is correct! I have lived many places in my life. These places include Washington, DC, Texas, South Carolina, and Virginia.';
+  numCorrect++;
+  alert(answerTwoY);
+  console.log(answerTwoY);
 } else if (secondQuestion.toUpperCase() === 'NO' || secondQuestion.toUpperCase() === 'N') {
-
-    alert('Actually I have been fortunate to live several different places in my life.');
-
-    console.log('Actually I have been fortunate to live several different places in my life.');
-
+  var answerTwoN = 'Actually I have been fortunate to live several different places in my life.';
+  alert(answerTwoN);
+  console.log(answerTwoN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var thirdQuestion = prompt('Did I attend college after graduating from high school?');
 
 if (thirdQuestion.toUpperCase() === 'YES' || thirdQuestion.toUpperCase() === 'Y') {
-
-    alert('That\'s correct.  I attended the University of Washington, and studied Political Science.');
-
-    console.log('That\'s correct.  I attended the University of Washington, and studied Political Science.');
-
+  var answerThreeY = 'That\'s correct.  I attended the University of Washington, and studied Political Science.';
+  numCorrect++;
+  alert(answerThreeY);
+  console.log(answerThreeY);
 } else if (thirdQuestion.toUpperCase() === 'NO' || thirdQuestion.toUpperCase() === 'N') {
-
-    alert('You are incorrect.  I did attend college after graduating from high school.');
-
-    console.log('You are incorrect.  I did attend college after graduating from high school.');
-
+  var answerThreeN = 'You are incorrect.  I did attend college after graduating from high school.';
+  alert(answerThreeN);
+  console.log(answerThreeN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var fourthQuestion = prompt('Do I have any siblings?');
 
 if (fourthQuestion.toUpperCase() === 'YES' || fourthQuestion.toUpperCase() === 'Y') {
-
-    alert('Yes. I do have siblings.');
-
-    console.log('Yes. I do have siblings.');
-
+  var answerFourY = 'Yes. I do have siblings.';
+  numCorrect++;
+  alert(answerFourY);
+  console.log(answerFourY);
 } else if (fourthQuestion.toUpperCase() === 'NO' || fourthQuestion.toUpperCase() === 'N') {
-
-    alert('Incorrect. I do have siblings.');
-
-    console.log('Incorrect. I do have siblings.');
-
+  var answerFourN = 'Incorrect. I do have siblings.';
+  alert(answerFourN);
+  console.log(answerFourN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
 
 var fifthQuestion = prompt('Am I right-handed?');
 
 if (fifthQuestion.toUpperCase() === 'YES' || fifthQuestion.toUpperCase() === 'Y') {
-
-    alert('No! I am left-handed.');
-
-    console.log('No! I am left-handed.');
-
+  var answerFiveY = 'No! I am left-handed.';
+  alert(answerFiveY);
+  console.log(answerFiveY);
 } else if (fifthQuestion.toUpperCase() === 'NO' || fifthQuestion.toUpperCase() === 'N') {
-
-    alert('That is correct!');
-
-    console.log('That is correct!');
-
+  var answerFiveN = 'That is correct!';
+  numCorrect++;
+  alert(answerFiveN);
+  console.log(answerFiveN);
 } else {
-
-    alert('You didn\'t enter a valid response!');
-
-    console.log('You didn\'t enter a valid response!');
+  alert(answerI);
+  console.log(answerI);
 }
+
+for (var i = 0; i < 4; i++) {
+
+  var sixthQuestion = prompt('Guess a number between 1 and 10.');
+
+  if (sixthQuestion < 7) {
+    var answerSixLow = 'The number you guessed is too low.';
+    alert(answerSixLow);
+    console.log(answerSixLow);
+  } else if (sixthQuestion > 7) {
+    var answerSixHigh = 'The number you guessed is too high.';
+    alert(answerSixHigh);
+    console.log(answerSixHigh);
+  } else if (sixthQuestion == 7) { // prompt returns a string, can't use functions
+    var answerSixCorrect = 'You are correct! The number was 7.';
+    numCorrect++;
+    alert(answerSixCorrect);
+    console.log(answerSixCorrect);
+    break;
+  } else {
+    var answerSixNaN = 'The answer you entered is not a number.';
+    alert(answerSixNaN);
+    console.log(answerSixNaN);
+  }
+}
+for (var i = 0; i < 6; i++) {
+
+  var favoriteMovies = ['Kong: Skull Island', 'Logan', 'Get Out', 'The Shack', 'Beauty and the Beast', 'Ghost in the Shell', 'The Lego Batman Movie', 'Moonlight', 'Hidden Figures', 'La La Land'];
+
+  var seventhQuestion = prompt('Can you name one of my top 10 favorite movies?');
+
+  var answerSevenCorrect = false;
+  for (var j = 0; j < favoriteMovies.length; j++) {
+
+    if (seventhQuestion.toUpperCase() == favoriteMovies[j].toUpperCase()) {
+      answerSevenCorrect = true;
+    }
+
+  }
+
+  if (answerSevenCorrect === true) {
+    var answerSevenY = 'That\'s right! ' + seventhQuestion + ' is one of my favorite movies.';
+    numCorrect++;
+    alert(answerSevenY);
+    console.log(answerSevenY);
+    break;
+  } else {
+    var answerSevenN = 'That is not correct. Acceptable answers include Kong: Skull Island, Logan, Get Out, The Shack, Beauty and the Beast, Ghost in the Shell, The Lego Batman Movie, Moonlight, Hidden Figures, and La La Land.';
+    alert(answerSevenN);
+    console.log(answerSevenN);
+  }
+
+}
+var totalCorrect = 'You correctly answered ' + numCorrect + ' out of 7 questions.';
+alert(totalCorrect);
+console.log(totalCorrect);
